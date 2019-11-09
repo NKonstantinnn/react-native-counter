a04627ee474da8ca0c9fcbfcb7162c246c683a01
kostya007 <kostya.1998.nosarev@mail.ru>
2019-11-09 13:49:20 +0700

    Add increment and decriment methods

--- components/ClassCounter.js
+++ components/ClassCounter.js
@@ -4,16 +4,32 @@ import {View, Text, TouchableOpacity} from 'react-native';
 import styles from './styles'
 
 class ClassCounter extends Component {
+    state = {
+        count: 0
+    }
+
+    inc = () => {
+        this.setState((prevState) => ({
+            count: prevState.count + 1
+        }));
+    }
+
+    dec = () => {
+        this.setState((prevState) => ({
+            count: prevState.count - 1
+        }));
+    }
+
     render() {
       return (
         <View style={styles.counter}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={this.dec}>
             <Text style={styles.counterTextButton}>-</Text>
           </TouchableOpacity>
   
-          <Text style={styles.counterCount}>0</Text>
+          <Text style={styles.counterCount}>{this.state.count}</Text>
   
-          <TouchableOpacity>
+          <TouchableOpacity onPress={this.inc}>
             <Text style={styles.counterTextButton}>+</Text>
           </TouchableOpacity>
         </View>
--- components/FuncCounter.js
+++ components/FuncCounter.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 
 import styles from './styles'
 
 const FuncCounter = () => {
+
+    const [count, setCount] = useState(() => (Math.round(Math.random() * 100)));
+
+    const inc = () => {
+        setCount((prevCount) => (prevCount + 1));
+    }
+
+    const dec = () => {
+        setCount((prevCount) => (prevCount - 1));
+    }
+
     return (
       <View style={styles.counter}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={dec}>
           <Text style={styles.counterTextButton}>-</Text>
         </TouchableOpacity>
   
-        <Text style={styles.counterCount}>0</Text>
+        <Text style={styles.counterCount}>{count}</Text>
   
-        <TouchableOpacity>
+        <TouchableOpacity onPress={inc}>
           <Text style={styles.counterTextButton}>+</Text>
         </TouchableOpacity>
       </View>
